3bb0e4752f3dbafe662d775672f4bffc3f9191bd
piotr-sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2023-04-21 17:53:06 +0000

    Przed dziedziczeniem
    Przed dziedziczeniem

--- main.cs
+++ main.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text;
+using System.Diagnostics;
 
 
 
@@ -19,14 +21,21 @@ class ArithmeticSequence{
 
     public override string ToString()
     {
-        string repr = "ArithmeticSequence(";
-        foreach (double term in terms)
+        StringBuilder sb = new StringBuilder();
+        sb.Append(this.GetType().Name);
+        sb.Append("(");
+       
+        for (int i = 0; i < terms.Length; i++)
         {
-            repr += term + ", ";
+            sb.Append(terms[i]);
+            if (i < terms.Length - 1)
+            {
+                sb.Append(", ");
+            }
         }
-        repr += "\b\b)";        
-
-        return repr;
+        
+        sb.Append(")");
+        return sb.ToString();
     }
 }
 
@@ -34,7 +43,32 @@ class GeometricSequence{
     double commonRatio = 2;
     double initialTerm;
     double[] terms = new double[12];   
-    
+
+    public GeometricSequence(in double initialTerm){
+        this.initialTerm = initialTerm;
+
+        this.terms[0] = this.initialTerm;
+        for(int i = 1; i < this.terms.Length; i++)
+            this.terms[i] += i * this.commonRatio * this.initialTerm;               
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(this.GetType().Name);
+        sb.Append("(");
+        for (int i = 0; i < terms.Length; i++)
+        {
+            sb.Append(terms[i]);
+            if (i < terms.Length - 1)
+            {
+                sb.Append(", ");
+            }
+        }
+        sb.Append(")");
+        
+        return sb.ToString();
+    }    
 }
 
 
@@ -42,6 +76,12 @@ class GeometricSequence{
 class Program {
     public static void Main (string[] args) {
         ArithmeticSequence arithmeticSequence = new ArithmeticSequence(2);
+        Trace.Assert(arithmeticSequence.ToString() == "ArithmeticSequence(0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22)");
         Console.WriteLine(arithmeticSequence.ToString());
+
+        
+        GeometricSequence geometricSequence = new GeometricSequence(2);
+        Trace.Assert(geometricSequence.ToString() == "GeometricSequence(2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44)");
+        Console.WriteLine(geometricSequence.ToString());
     }    
 }
