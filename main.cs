9b645b530f844b0b0e05ae01c7c364f61b6c04b8
piotr-sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2023-04-21 18:00:28 +0000

    Dodana klasa bazowa

--- main.cs
+++ main.cs
@@ -2,22 +2,9 @@ using System;
 using System.Text;
 using System.Diagnostics;
 
-
-
-class ArithmeticSequence{
-    private double difference;
-    private double initialTerm;
-    private double[] terms = new double[12];
-
-    public ArithmeticSequence(in double difference)
-    {
-        this.initialTerm = 0;
-        this.difference = difference;       
-
-        this.terms[0] = this.initialTerm;
-        for(int i = 1; i < this.terms.Length; i++)
-            this.terms[i] += i * this.difference + this.initialTerm;        
-    }
+class Sequence{
+    protected double initialTerm;
+    protected double[] terms = new double[12];
 
     public override string ToString()
     {
@@ -36,13 +23,25 @@ class ArithmeticSequence{
         
         sb.Append(")");
         return sb.ToString();
-    }
+    }    
 }
 
-class GeometricSequence{
+class ArithmeticSequence : Sequence{
+    private double difference;
+    
+    public ArithmeticSequence(in double difference)
+    {
+        this.initialTerm = 0;
+        this.difference = difference;       
+
+        this.terms[0] = this.initialTerm;
+        for(int i = 1; i < this.terms.Length; i++)
+            this.terms[i] += i * this.difference + this.initialTerm;        
+    }   
+}
+
+class GeometricSequence: Sequence{
     double commonRatio = 2;
-    double initialTerm;
-    double[] terms = new double[12];   
 
     public GeometricSequence(in double initialTerm){
         this.initialTerm = initialTerm;
@@ -51,24 +50,6 @@ class GeometricSequence{
         for(int i = 1; i < this.terms.Length; i++)
             this.terms[i] += i * this.commonRatio * this.initialTerm;               
     }
-
-    public override string ToString()
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.Append(this.GetType().Name);
-        sb.Append("(");
-        for (int i = 0; i < terms.Length; i++)
-        {
-            sb.Append(terms[i]);
-            if (i < terms.Length - 1)
-            {
-                sb.Append(", ");
-            }
-        }
-        sb.Append(")");
-        
-        return sb.ToString();
-    }    
 }
 
 
