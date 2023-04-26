e1e5b5662132d6fb666f9a536c346be4f9e2f505
Piotr Sobieraj <psobieraj+github@gmail.com>
2023-04-26 20:50:37 +0000

    Added reverse

--- Models/GeometricSequence.cs
+++ Models/GeometricSequence.cs
@@ -1,13 +1,13 @@
 namespace Models{
     class GeometricSequence : Sequence{
-        double commonRatio = 2;
+        double commonRatio = -2;
     
         public GeometricSequence(in double initialTerm){
             this.initialTerm = initialTerm;
     
             this.terms[0] = this.initialTerm;
             for(int i = 1; i < this.terms.Length; i++)
-                this.terms[i] += i * this.commonRatio * this.initialTerm;               
+                this.terms[i] += this.commonRatio * this.terms[i - 1];               
         }
     }    
 }
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -9,20 +9,21 @@ namespace Tests{
             Console.WriteLine("##### Starting tests of the ArithmeticSequence class #####");
             
             ArithmeticSequence seq1 = new ArithmeticSequence(difference: 0);
-            Console.WriteLine("GIVEN arithmetic sequence where initial term is 0 and the difference is 0");            
+            Console.WriteLine("GIVEN arithmetic sequence with initial term of 0 and the difference of 0");            
             Trace.Assert(seq1.ToString() == "ArithmeticSequence(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)");
-            Console.WriteLine("THEN the sequence is 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0");            
+            Console.WriteLine("THEN the generated sequence is 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0");            
             Trace.Assert(seq1.Mean == 0);
-            Console.WriteLine("AND the mean is 0");    
+            Console.WriteLine("AND its mean is 0");    
             Console.WriteLine();
             
 
             ArithmeticSequence seq2 = new ArithmeticSequence(difference: -1.5);
-            Console.WriteLine("GIVEN arithmetic sequence where initial term is 0 and the difference is -1.5");            
+            Console.WriteLine("GIVEN arithmetic sequence with the initial term of 0 and the difference of -1.5");            
+            
             Trace.Assert(seq2.ToString() == "ArithmeticSequence(0, -1.5, -3, -4.5, -6, -7.5, -9, -10.5, -12, -13.5, -15)");
-            Console.WriteLine("THEN the sequence is 0, -1.5, -3, -4.5, -6, -7.5, -9, -10.5, -12, -13.5, -15");            
+            Console.WriteLine("THEN the generated sequence is 0, -1.5, -3, -4.5, -6, -7.5, -9, -10.5, -12, -13.5, -15");            
             Trace.Assert(seq2.Mean == -7.5);
-            Console.WriteLine("AND the mean is -7.5");            
+            Console.WriteLine("AND its mean is -7.5");            
         }
         
     }
@@ -30,20 +31,21 @@ namespace Tests{
     class TestGeometricSequence{
         public TestGeometricSequence(){
             Console.WriteLine("\n\n##### Starting tests of the GeometricSequence class #####");
-
-            GeometricSequence seq1 = new GeometricSequence(initialTerm: 0);    
-            Console.WriteLine("GIVEN geometric sequence where initial term is 0 and the common ratio is 2");            
-            Trace.Assert(seq1.ToString() == "GeometricSequence(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)");
-            Console.WriteLine("THEN the sequence is 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0");            
-            Trace.Assert(seq1.Mean == 0);
-            Console.WriteLine("AND the mean is 0");     
+            GeometricSequence seq1 = new GeometricSequence(initialTerm: 1);    
+            
+            Console.WriteLine("GIVEN geometric sequence with initial term of 1 and the common ratio of -2");            
+            Trace.Assert(seq1.ToString() == "GeometricSequence(1, -2, 4, -8, 16, -32, 64, -128, 256, -512, 1024)");
+            Console.WriteLine("THEN the generated sequence is 1, -2, 4, -8, 16, -32, 64, -128, 256, -512, 1024");            
+            Trace.Assert(seq1.Mean == 62.09090909090909);
+            Console.WriteLine("AND its mean is 62.09090909090909");     
             
             GeometricSequence seq2 = new GeometricSequence(initialTerm: -1.5);
-            Console.WriteLine("GIVEN geometric sequence where initial term is -1.5 and the common ratio is 2");
-            Trace.Assert(seq2.ToString() == "GeometricSequence(-1.5, -3, -6, -9, -12, -15, -18, -21, -24, -27, -30)");
-            Console.WriteLine("THEN the sequence is -1.5, -3, -6, -9, -12, -15, -18, -21, -24, -27, -30");            
-            Trace.Assert(seq2.Mean == -15.136363636363637);
-            Console.WriteLine("AND the mean is -15.136363636363637");     
+            
+            Console.WriteLine("GIVEN geometric sequence with initial term of -1.5 and the common ratio of -2");
+            Trace.Assert(seq2.ToString() == "GeometricSequence(-1.5, 3, -6, 12, -24, 48, -96, 192, -384, 768, -1536)");
+            Console.WriteLine("THEN the generated sequence is -1.5, 3, -6, 12, -24, 48, -96, 192, -384, 768, -1536)");            
+            Trace.Assert(seq2.Mean == -93.13636363636364);
+            Console.WriteLine("AND its mean is -93.13636363636364");     
         }
     }    
 
@@ -55,28 +57,26 @@ namespace Tests{
             ZippedSequence seq3 = new ZippedSequence(seq1, seq2);
 
             Console.WriteLine("GIVEN arithmetic sequence 0.0, 0.75, 1.5, 2.25, 3.0, 3.75, 4.5, 5.25, 6.0, 6.75, 7.5");
-            Console.WriteLine("AND   geometric sequence  0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0");
+            Console.WriteLine("AND   geometric sequence  0.5, -1, 2, -4, 8, -16, 32, -64, 128, -256, 512");
             Console.WriteLine("WHEN their terms are added one by one");
-            Trace.Assert(seq3.ToString() == "ZippedSequence(0.5, 1.75, 3.5, 5.25, 7, 8.75, 10.5, 12.25, 14, 15.75, 17.5)");            
-            Console.WriteLine("THEN the outcome sequence is 0.5, 1.75, 3.5, 5.25, 7.0, 8.75, 10.5, 12.25, 14.8, 15.75, 17.5");
-            Trace.Assert(seq3.Mean == 8.795454545454545);
-            Console.WriteLine("AND mean of the new sequence is 8.795454545454545");
+            Trace.Assert(seq3.ToString() == "ZippedSequence(0.5, -0.25, 3.5, -1.75, 11, -12.25, 36.5, -58.75, 134, -249.25, 519.5)");            
+            Console.WriteLine("THEN the new sequence is 0.5, -0.25, 3.5, -1.75, 11, -12.25, 36.5, -58.75, 134, -249.25, 519.5");
+            Trace.Assert(seq3.Mean == 34.79545454545455);
+            Console.WriteLine("AND mean of the new sequence is 34.79545454545455");
        }
     }
 
 
     class TestReversedSequence{
         public TestReversedSequence(){
-            Console.WriteLine("\n\n##### Starting tests of the  Sequence.Reverse method #####");
-            GeometricSequence geom = new GeometricSequence(initialTerm: 1.125);
-            
+            Console.WriteLine("\n\n##### Starting tests of the Sequence.Reverse method #####");
+            GeometricSequence geom = new GeometricSequence(initialTerm: 1.125);            
 
-            Console.WriteLine("GIVEN geometric sequence 1.125, 2.25, 4.5, 6.75, 9, 11.25, 13.5, 15.75, 18, 20.25, 22.5");
+            Console.WriteLine("GIVEN geometric sequence 1.125, -2.25, 4.5, -9, 18, -36, 72, -144, 288, -576, 1152");
             Sequence rev_geom = geom.Reverse();                        
             Console.WriteLine("WHEN a new sequence in reversed order is created");
-            Trace.Assert(rev_geom.ToString() == "Sequence(22.5, 20.25, 18, 15.75, 13.5, 11.25, 9, 6.75, 4.5, 2.25, 1.125)");            
-            Console.WriteLine("THEN the new sequence is 22.5, 20.25, 18, 15.75, 13.5, 11.25, 9, 6.75, 4.5, 2.25, 1.125");            
+            Trace.Assert(rev_geom.ToString() == "Sequence(1152, -576, 288, -144, 72, -36, 18, -9, 4.5, -2.25, 1.125)");            
+            Console.WriteLine("THEN the new sequence is 1152, -576, 288, -144, 72, -36, 18, -9, 4.5, -2.25, 1.125");            
+        }
     }
 }
-}
-
